c0c79fd300a051c559fb2de0ec7aba6825cb00e7
adithyanrk67 <adithyanrk67@gmail.com>
2021-07-17 13:47:09 +0530

    First commit

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+     <title>Naksha Traders</title>
+     <link rel="stylesheet" type="text/css"href="css/style.css">
+    
+	
+</head>
+<body>
+      <header>
+            <div class="main">
+               <ul> 
+                    <li class="active"><a href="#">HOME</a></li>
+                    <li><a href="#">ABOUT US</a></li>
+                    <li><a href="#">OUR PRODUCTS</a></li>
+                    <li><a href="#">SERVICES</a></li>
+                    <li><a href="#">Admin</a></li>
+                    
+               </ul>  
+             </div>
+	<div class="title"> 
+	 <h1></h1>
+           	</div>
+            
+
+</body>
+</html>
